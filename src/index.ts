8351235a15dce5949a46d74b6c81706d2c46f9ba
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-09-11 23:01:38 +0100

    Changed 'connectionStr' cookie's MaxAge Settings and changed default sql query on connect

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,6 @@ function logRequestDetails(req, res, next) {
 	next()
 }
 
-// TODO: set proper time intervals as well as proper cookie parameters
-
-
 function setCorsHeaders(req, res, next) {
 	res.set('Access-Control-Allow-Origin', 'http://localhost:5173') // TODO: make it environment dependent
 	res.set('Access-Control-Allow-Headers', 'Content-Type')
@@ -35,12 +32,14 @@ async function processReq(connectionStr) {
 		client = await pool.connect();
 	}catch(error) {
 		// add better error message i.e server doesn't support ssl, invalid connection strings
+		console.log(error)
 		return {statusCode: 400, info: {errorMsg: error.message, data: null}};
 	} 
 
 	let data = null;
 	try {
-		data = await client.query("SELECT table_name FROM information_schema.tables where table_schema = 'public';");
+		// SELECT table_name FROM information_schema.tables where table_schema = 'public';
+		data = await client.query("SELECT datname FROM pg_database; SELECT rolname FROM pg_roles;");
 		return {statusCode: 200, info: {errorMsg: null, data}};
 	}catch(error) {
 		console.log(error)
@@ -75,7 +74,7 @@ app.post('/connect-db', async (req, res, next) => {
 
 	if (connectResults.statusCode == 200) {
 		res.cookie('connectionStr', req.body.str, 
-		{httpOnly: true, secure: false, maxAge: 3600000} // is this sec or msec?
+		{httpOnly: true, secure: true, maxAge: 604800} // 7 days
 		)
 	}
 	res.status(connectResults.statusCode).json(connectResults.info)
