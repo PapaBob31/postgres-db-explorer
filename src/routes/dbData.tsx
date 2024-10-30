cff519d4849f38d45e929d69c4c98aaafa5e41f6
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-10-30 11:17:11 +0100

    Implemented update table feature

--- src/routes/dbData.tsx
+++ src/routes/dbData.tsx
@@ -13,7 +13,6 @@ function TablesWithSameColumnName({columnName, tablesData, updateDisplay}
   let targetTables: JSX.Element[] = [];
   function getAndUpdateData(targetTable: string) {
     const query = `SELECT * FROM "${tableDetails.tableName}" INNER JOIN ${targetTable} USING("${columnName}");`
-    console.log(query);
     getData(tableDetails.targetDb, query)
     .then(response => {
       if (!response.errorMsg) {
@@ -82,12 +81,11 @@ function TableHeader({ headersList, tablesData, updateDisplay } :
   return <tr>{htmlElements}</tr>;
 }
 
-// type paramType = {displayName: string, data: GenericQueryData};
 interface QueryRow {
   [key: string]: string
 }
 
-function Row({rowData, headersList, updateData} : { rowData: QueryRow, headersList: string[], updateData: (exc: string)=>void}) {
+function DataRow({rowData, headersList, updateData} : { rowData: QueryRow, headersList: string[], updateData: (exc: string)=>void}) {
   const rowId = useRef("");
   const {targetDb, schemaName, tableName} = useContext(TopLevelTableDetails)
   let row:any = [];
@@ -119,7 +117,7 @@ function Row({rowData, headersList, updateData} : { rowData: QueryRow, headersLi
     })
   }
 
-  return <tr>{row}<button onClick={deleteAction}>Delete</button><button>update</button></tr>
+  return <tr>{row}<td><button onClick={deleteAction}>Delete</button></td></tr>
 
 }
 
@@ -127,23 +125,173 @@ function Cell({cellData} : {cellData: string} ) {
   return <td>{cellData}</td>
 }
 
-function TableBody({data} : {data: GenericQueryData}) {
+function InputCell({currentData, columnDataTypeMap, columnName, syncUpdates} : 
+  {currentData: string, columnDataTypeMap: {[key: string]: string}, columnName: string, syncUpdates: (param1: string, param2: string) => void}) {
+
+  const [inputState, setInputState] = useState("")
+
+  const numericTypes = ["bigint", "integer", "smallint", "numeric", "real", "double precision", "serial"]; // int2, int4, int8 ?
+  const inputType = numericTypes.includes(columnDataTypeMap[columnName]) ? "number" : "text"
+
+  function processUserInput(event) {
+    if (inputType === "number" && !(/^\d+$/).test(event.target.value)) { // TODO: Add more robust input type validation
+      setInputState("error")
+    }else {
+      let input = ""
+      if (inputType !== "number") {
+        input = `'${event.target.value}'`
+      }else input = event.target.value;
+      syncUpdates(columnName, input)
+    }
+  }
+  return <td><input type={inputType} className={inputState} defaultValue={currentData} onBlur={processUserInput} onFocus={() => setInputState("")} /></td>
+}
+
+interface DataUpdate {
+  ctid: string;
+  updates: [string, string][]
+}
+
+function InputRow({rowData, headersList, updateData, columnData, rowDataIndex} : 
+  { rowData: QueryRow, headersList: string[], updateData: (dat: DataUpdate, i: number)=>void, columnData: any, rowDataIndex: number}) {
+
+  const rowId = useRef("");
+  // const {targetDb, schemaName, tableName} = useContext(TopLevelTableDetails)
+  let row:any = [];
+  const rowEditDetails = useRef<DataUpdate>({ctid: "", updates: []});
+  const columnDataMap: {[key: string]: string} = {};
+  const numericTypes = ["bigint", "integer", "smallint", "numeric", "real", "double precision", "serial"]; // int2, int4, int8 ?
+
+
+  for (let data of columnData.rows) {
+    columnDataMap[data["column_name"]] = data["data_type"]
+  }
+
+  function syncUpdates(columnName: string, newValue: string) {
+    let actualNewValue: any = ""
+    if (numericTypes.includes(columnDataMap[columnName])) {
+      actualNewValue = parseInt(newValue)
+    }
+    let dataChanged:boolean = rowData[columnName] !== actualNewValue;
+
+    if (dataChanged) {
+      let update;
+      for (let data of rowEditDetails.current.updates) {
+        if (data[0] === columnName) {
+          update = data
+        }
+      }
+      if (!update) {
+        rowEditDetails.current.updates.push([])
+        update = rowEditDetails.current.updates[rowEditDetails.current.updates.length-1]
+      }
+      update[0] = columnName
+      update[1] = newValue
+    }else {
+      rowEditDetails.current.updates = rowEditDetails.current.updates.filter(update => update[0] !== columnName)
+      console.log(rowEditDetails.current.updates)
+    }
+    updateData(rowEditDetails.current, rowDataIndex)
+  }
+
+  for (let i=0; i<headersList.length; i++) {
+    if (headersList[i] === "ctid"){
+      rowEditDetails.current.ctid = rowData[headersList[i]];
+      rowId.current = rowData[headersList[i]]
+      continue;
+    }
+    row.push(
+      <InputCell key={i} currentData={rowData[headersList[i]]} columnName={headersList[i]} 
+        columnDataTypeMap={columnDataMap} syncUpdates={syncUpdates}/>
+    ) // empty cells??
+  }
+
+  return <tr>{row}</tr>
+}
+
+function initArray(length: number) {
+  const data: DataUpdate[] = Array(length);
+
+  for (let i=0; i<length; i++) {
+    data[i] = {ctid: "", updates: []}
+  }
+  return data;
+}
+
+
+function columnUpdatesStr(updates: [string, string][]) {
+  let querySubstr = ""
+  updates.forEach(update => {
+    if (querySubstr)
+      querySubstr += ', ';
+    querySubstr += `"${update[0]}" = ${update[1]}`
+  })
+  return querySubstr
+}
+
+function constructQuery(dataInput: DataUpdate[], tableName: string) {
+  let query = ""
+  dataInput.forEach(data => {
+    if (data.ctid && data.updates.length > 0) {
+      query += `UPDATE "${tableName}" SET ${columnUpdatesStr(data.updates)} WHERE ctid = '${data.ctid}';` 
+    }
+  })
+  return query;
+}
+
+// getData is very similar to this except the endpoint, I should probably edit it to accept endpoints as parameters
+function sendUpdateQuery(targetDb: string, query: string) {
+  return fetch("http://localhost:4900/update-table", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "include",
+    method: "POST",
+    body: JSON.stringify({targetDb, query}) // TODO: limit the amount of data sent back
+  }).then(response => response.json())
+}
+
+function TableBody({data, editMode, columnData, updateDisplay} : {data: GenericQueryData, columnData: GenericQueryData, editMode: boolean, updateDisplay: (d:any)=>void}) {
   const [rowsData, setRowsData] = useState(data.rows);
-  const rows = rowsData.map((entity, index) => <Row key={index} rowData={entity} headersList={data.fields} updateData={updateRows}/>)
+  const {tableName, targetDb, schemaName } = useContext(TopLevelTableDetails)
+  let rows:JSX.Element[] = [];
+  const editedRowsData = useRef<DataUpdate[]>(initArray(rowsData.length));
+
+  function updateEditedRowsData(newData: {ctid: string, updates: [string, string][]}, index: number) {
+    editedRowsData.current[index] = newData;
+  }
+
   function updateRows(excludedValue: string) {
     setRowsData(rowsData.filter(data => data["ctid"] !== excludedValue))
   }
 
-  // let rows:any = [];
-  // let rowData:any[] = [];
-  // data.forEach(entity => {
-  //   for (let i=0; i<headersList.length; i++) {
-  //     rowData.push(<td key={i}>{entity[headersList[i]]}</td>) // empty cells??
-  //   }
-  //   rows.push(<tr>{rowData}</tr>);
-  //   rowData = [];
-  // })
-  return <tbody>{rows}</tbody>
+  function updateTable() {
+    if (editedRowsData.current.some(data => data.updates.length > 0)) {
+      // todo: Implement not sending on input validation error from InputCell
+      const updateQuery = constructQuery(editedRowsData.current, tableName)
+      sendUpdateQuery(targetDb, updateQuery+`SELECT ctid, * FROM "${schemaName}"."${tableName}";`)
+      .then(response => {
+        if (response.errorMsg) {
+          alert(response.errorMsg)
+        }else {
+          updateDisplay(response.data)
+          setRowsData(response.data.rows)
+        }
+      })
+      // console.log(updateQuery)
+    }
+  }
+
+  if (editMode) {
+    rows = rowsData.map((entity, index) => (
+      <InputRow key={index} rowData={entity} rowDataIndex={index}
+        headersList={data.fields} updateData={updateEditedRowsData} 
+        columnData={columnData}
+      />))
+  }else rows = rowsData.map((entity, index) => <DataRow key={index} rowData={entity} headersList={data.fields} updateData={updateRows}/>)
+
+
+  return <tbody>{rows}{editMode && <tr><td><button onClick={updateTable}>Update</button></td></tr>}</tbody>
 }
 
 function Table({schemaName, tableName, setVisibleMenu, visibleMenu}:{schemaName: string, tableName: string, setVisibleMenu: (menu: string)=>void, visibleMenu: string}) {
@@ -308,15 +456,13 @@ interface TableDetails {
   schemaName: string
 }
 
-function displayColumns(setDisplayData: (data: any) => void, queryDetails: TableDetails) {
-  const {schemaName, tableName, targetDb} = queryDetails;
-  // Get the details from the db everytime as values may have changed since the last time you checked
-  const query = `SELECT column_name, data_type FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '${tableName}' AND table_schema = '${schemaName}';`
-  getData(targetDb, query)
-  .then(responseData => {
-    if (responseData.errorMsg)
-      alert("Error occurred while trying to query db!");
-    setDisplayData({displayName: "Table Columns", data: responseData.data})});
+
+function getTableColumns(data: GenericQueryData, tableName: string) {
+  // It seems like firefox only reevaluates the result of filter functions after in consoles if the condition changes idk
+  let newData: GenericQueryData = {rows: [], fields: ["column_name", "data_type"]};
+  newData.rows = JSON.parse(JSON.stringify(data.rows.filter(row => row["table_name"] === tableName)))
+  newData.rows.forEach((row) => {delete row["table_name"]; delete row["table_schema"]});
+  return newData
 }
 
 function sendDropTableReq(dbName: string, tableName: string, cascade: boolean): Promise<{errorMsg: string|null}> {
@@ -333,26 +479,35 @@ function sendDropTableReq(dbName: string, tableName: string, cascade: boolean): 
 
 export function TableDisplay({tableDetails, displayType} : {tableDetails: TableDetails, displayType: string}) {
   const [displayData, setDisplayData] = useState<{displayName: string, data: GenericQueryData}|null>(null); // add display type on errors
+  const [editMode, setEditMode] = useState("");
   const allTableDetails = useRef<GenericQueryData|null>(null)
   const updateMainDisplay = useContext(DataDisplayFn)
 
   useEffect(() => {
-    if (displayType === "Table Columns"){
-      displayColumns(setDisplayData, tableDetails);
-      return;
-    }
+
     const longQuery = `SELECT column_name, data_type, table_name, table_schema FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '${tableDetails.schemaName}';`
     const qualifiedTableName = `"${tableDetails.schemaName}"."${tableDetails.tableName}"`;
-    Promise.all([getData(tableDetails.targetDb, `SELECT ctid, * FROM ${qualifiedTableName};`), getData(tableDetails.targetDb, longQuery)])
+    Promise.all([
+      getData(tableDetails.targetDb, `SELECT ctid, * FROM ${qualifiedTableName};`), 
+      getData(tableDetails.targetDb, longQuery)
+    ])
     .then(values => {
       if (values[0].errorMsg){
-        alert("Error occurred while trying to query db!");
-        // todo: set state for some kind of error
+        alert("Error occurred while trying to query db!") // todo: set state for some kind of error
         return
-      }else setDisplayData({displayName: "Table Rows", data: values[0].data})
-      if(!values[1].errorMsg) {
+      }
+      if (values[1].errorMsg) {
+        alert("Error occurred while trying to query db! @ allTableDetails");
+        return
+      }else {
         allTableDetails.current = values[1].data
-      }else alert("Error occurred while trying to query db! @ allTableDetails");
+      }
+      if (displayType === "Table Columns") {
+        const columnData = getTableColumns(allTableDetails.current as GenericQueryData, tableDetails.tableName) // maybe usememo it if expensive
+        setDisplayData({displayName: "Table Columns", data: columnData});
+      }else { // Table Rows
+        setDisplayData({displayName: "Table Rows", data: values[0].data})
+      }
     });
   }, [displayType])
 
@@ -366,12 +521,23 @@ export function TableDisplay({tableDetails, displayType} : {tableDetails: TableD
     }
   }
 
+  function tableBodyUpdateFn(newData: GenericQueryData) {
+    if (editMode === "rows") {
+      setEditMode("")
+    }
+    setDisplayData({displayName: "Table Rows", data: newData})
+  }
+  function changeEditMode() {
+    setEditMode(editMode === "rows" ? "" : "rows")
+  }
 
   return (
     <section>
       {displayData && displayData.displayName !== "loading" ? (
         <TopLevelTableDetails.Provider value={tableDetails}>
           <h1>{displayData.displayName}</h1>
+          <button className={editMode === "rows" ? "active" : ""} onClick={changeEditMode}>Rows edit mode</button>
+          <button disabled>Find and Replace</button>
           <table>
             <thead>
               <TableHeader 
@@ -379,7 +545,11 @@ export function TableDisplay({tableDetails, displayType} : {tableDetails: TableD
                 tablesData={allTableDetails.current as GenericQueryData}
                 updateDisplay={setDisplayData} />
             </thead>
-            <TableBody data={displayData.data}/>
+            <TableBody 
+              data={displayData.data}
+              updateDisplay={tableBodyUpdateFn}
+              editMode={editMode === "rows"}
+              columnData={getTableColumns(allTableDetails.current as GenericQueryData, tableDetails.tableName)}/>
           </table>
           <button onClick={() => updateMainDisplay({type: "insert-form", data: tableDetails})}>insert</button>
           <button onClick={() => dropTableAction(tableDetails, true)}>Drop Table Cascade</button>
