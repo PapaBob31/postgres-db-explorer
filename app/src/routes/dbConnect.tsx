774c24e8139d551232634274a5999bb0a2c7dc4e
Adedamola Egbebi <egbebitimi121@gmail.com>
2024-12-15 23:58:10 +0100

    added interface for 'connection uri' connect interface, implemented saving server config

--- app/src/routes/dbConnect.tsx
+++ app/src/routes/dbConnect.tsx
@@ -1,27 +1,35 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { newServerConnected, tabCreated } from "../store"
 import { useDispatch } from "react-redux"
 
 
 interface URLParams {
-	serverName: string,
+	servername: string,
 	user: string;
 	password: string;
 	hostname: string;
 	dbname: string;
 	port: number;
 	saveConnDetails: boolean;
-	ssl: boolean
+	ssl: boolean;
+	connectionUri: string;
 }
 
-async function connectDb(urlParams: URLParams) {
-	let connectionString = `postgresql://${urlParams.user}:${urlParams.password}@${urlParams.hostname}:${urlParams.port}/${urlParams.dbname}`
+async function connectDb(urlParams: URLParams, saveConnDetails: boolean) {
+	const payload:any = {servername: urlParams.servername, saveConnDetails}
+
+	if (urlParams.connectionUri) {
+		payload.isConnUri = true
+		payload.connectionDetails = urlParams.connectionUri
+	}else {
+		payload.connectionDetails = urlParams;
+	}
 
 	const serverReq = new Request("http://localhost:4900/connect-db", {
 		credentials: "include",
 		headers: {"Content-Type": "application/json"},
 		method: "POST",
-		body: JSON.stringify({connectionString, serverName: urlParams.saveConnDetails ? urlParams.serverName : null, ssl: urlParams.ssl})
+		body: JSON.stringify({payload})
 	})
 
 	let response;
@@ -37,10 +45,11 @@ async function connectDb(urlParams: URLParams) {
 function serializeForm(elem: Element) {
 	let urlParamsObj = Object();
 	const inputElements = elem.querySelectorAll("input");
+
 	for (let element of inputElements) {
 		if (element.name === "ssl")
 			urlParamsObj[element.name] = element.checked;
-		else
+		else if (element.name !== "saveConnDetails")
 			urlParamsObj[element.name] = element.value;
 	}
 	return urlParamsObj
@@ -64,41 +73,77 @@ function genDashBoardStatePayload(obj: URLParams, connectionString: string) {
 export default function ConnectDbForm() {
 	const formRef = useRef<HTMLFormElement|null>(null)
 	const dispatch = useDispatch();
+	const [display, setDisplay] = useState<"Connection Details"|"Connection URI">("Connection Details")
+	const [saveConnDetails, setSaveConnDetails]  = useState(true)
 
 	async function connect(event: React.FormEvent) {
 		event.preventDefault()
 		let urlParams = serializeForm(formRef.current as HTMLFormElement)
-		const response = await connectDb(urlParams);
+		const response = await connectDb(urlParams, saveConnDetails);
 		if (!response) return;
+
 
 		if (response.ok) {
 			const connString = `postgresql://${urlParams.user}:${urlParams.password}@${urlParams.hostname}:${urlParams.port}`
-			const newTabPayload = genDashBoardStatePayload(urlParams, connString)
+			const newTabPayload = genDashBoardStatePayload(urlParams, connString, )
 			dispatch(newServerConnected({name: "unimplemented yet", connString, connected: true, connectedDbs: [urlParams.dbname]}))
 			dispatch(tabCreated(newTabPayload))
 		}else {
 			alert("Invalid connection string")
 		}
 	}
+
 	return (
-		<form id="server-connect-form" method="post" ref={formRef} onSubmit={connect}>
-			<label>Server Name</label>
-			<input type="text" name="serverName" required/>
-			<label>User</label>
-			<input type="text" name="user" required/>
-			<label>Password</label>
-			<input type="password" name="password" required/>
-			<label>Host name</label>
-			<input type="text" name="hostname" required/>
-			<label>Port number</label>
-			<input type="number" name="port" min="1024" max="65535" required/>
-			<label>Db name</label>
-			<input type="text" name="dbname"/>
-			<label>SSl</label>
-			<input type="checkbox" name="ssl"/>
-			<label>Save connection details</label>
-			<input type="checkbox" name="saveConnDetails" />
-			<button type="submit">Connect</button>
-		</form>
+		<section>
+			<button 
+				onClick={() => setDisplay("Connection Details")} 
+				className={display === "Connection Details" ? "active" : "" }>
+				Connection Details
+			</button>
+			<button 
+				onClick={() => setDisplay("Connection URI")}
+				className={display === "Connection URI" ? "active" : "" }>
+				Connection URI
+			</button>
+			{display === "Connection Details" ? 
+				<form id="server-connect-form" method="post" ref={formRef} onSubmit={connect}>
+					<h1>Connection parameters</h1>
+					<label>Server Name</label>
+					<input type="text" name="servername" maxLength={50} required/>
+					<label>User</label>
+					<input type="text" name="user"/>
+					<label>Password</label>
+					<input type="password" name="password"/>
+					<label>Host name (<em>not ip addr</em>)</label>
+					<input type="text" name="hostname"/>
+					<label>Port number</label>
+					<input type="number" name="port" min="1024" max="65535"/>
+					<label>Db name</label>
+					<input type="text" name="dbname"/>
+					<label>SSl</label>
+					<input type="checkbox" name="ssl"/>
+					<label>
+						<span>Save connection details </span>
+						<button title="save connnection details for easier connection next time">info</button>
+					</label>
+					<input type="checkbox" checked={saveConnDetails} name="saveConnDetails" onClick={() => setSaveConnDetails(!saveConnDetails)} />
+					<button type="submit">Connect</button>
+				</form>
+			:
+				<form ref={formRef}>
+					<h1>Connection URI</h1>
+					<label>Server Name</label>
+					<input type="text" name="servername" maxLength={50} required/>
+					<label>URI</label>
+					<input type="text" name="connectionUri" required/>
+					<label>
+						<span>Save connection details </span>
+						<button title="save connnection details for easier connection next time">info</button>
+					</label>
+					<input type="checkbox" checked={saveConnDetails} name="saveConnDetails" onClick={() => setSaveConnDetails(!saveConnDetails)} />
+					<button type="submit">Connect</button>
+				</form>
+			}
+		</section>
 	)
 }
